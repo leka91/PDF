51cab3fab377e304330771ac229dbf45bea0444a
Aleksandar Krasic <krasicaleksandar1@gmail.com>
2021-01-11 23:04:32 +0100

    .

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('SIGNATURE_POSITION_X', 148);
+define('SIGNATURE_POSITION_Y', 220);
+define('SIGNATURE_WIDTH', 36);
+define('SIGNATURE_HEIGHT', 15);
+
+define('PAYEMENT_CODE_POSITION_X', 44);
+define('PAYEMENT_CODE_POSITION_Y', 115);
+define('PAYEMENT_CODE', 302);
+
+define('FONT_FAMILY', 'Helvetica');
+define('BOLD', null);
+define('FONT_SIZE', 12);
