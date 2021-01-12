a7d5e0a97c83368ade90312638accfb19fc34822
Aleksandar Krasic <krasicaleksandar1@gmail.com>
2021-01-12 16:23:28 +0100

    .

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define('SOURCE_FILE', glob('./*.pdf')[0]);
 
-define('SIGNATURE', './signature.png');
+define('SIGNATURE', glob('./*.png')[0]);
 define('SIGNATURE_POSITION_X', 148);
 define('SIGNATURE_POSITION_Y', 220);
 define('SIGNATURE_WIDTH', 36);
